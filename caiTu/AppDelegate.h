96fd06ceeacb7049e49cf26a62086998c3190545
admin <320542915@qq.com>
2014-10-28 10:00:14 +0800

    Initial Commit

---
+++ caiTu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  caiTu
+//
+//  Created by admin on 14-10-28.
+//  Copyright (c) 2014年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ caiTuTests/caiTuTests.m
@@ -0,0 +1,36 @@
+//
+//  caiTuTests.m
+//  caiTuTests
+//
+//  Created by admin on 14-10-28.
+//  Copyright (c) 2014年 xl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface caiTuTests : XCTestCase
+
+@end
+
+@implementation caiTuTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
