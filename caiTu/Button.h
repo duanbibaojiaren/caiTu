3e561fa27e54d3968843c95409ffbfac960b3541
admin <320542915@qq.com>
2014-10-28 10:04:04 +0800

    bb

---
+++ caiTu/Button.h
@@ -0,0 +1,13 @@
+//
+//  Button.h
+//  caiTu
+//
+//  Created by admin on 14-10-28.
+//  Copyright (c) 2014年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Button : UIButton
+
+@end
